1bc6db403192476bd6001aec7a4e62815ad2622b
Alexander Erlemann <alexander.erlemann@worldiety.com>
2018-04-19 17:00:41 +0200

    Fixed broken padding, published 0.100

--- hcf-android-component/src/main/java/org/homunculus/android/component/InflaterView.java
+++ hcf-android-component/src/main/java/org/homunculus/android/component/InflaterView.java
@@ -4,41 +4,23 @@ import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 /**
  * A basic class which is mainly used by code generator to create type safe views from XML files.
  * <p>
  * Created by Torben Schinke on 22.02.18.
  */
-public abstract class InflaterView extends ViewGroup {
-    private final View inflatedChild;
+public abstract class InflaterView extends FrameLayout {
 
     public InflaterView(Context context, @LayoutRes int layoutId) {
         super(context);
         LayoutInflater li = LayoutInflater.from(getContext());
-        inflatedChild = li.inflate(layoutId, null);
-        super.addView(inflatedChild);
-    }
-
-    @Override
-    public void setLayoutParams(LayoutParams params) {
-        super.setLayoutParams(params);
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        inflatedChild.measure(widthMeasureSpec, heightMeasureSpec);
-        setMeasuredDimension(inflatedChild.getMeasuredWidth(), inflatedChild.getMeasuredHeight());
+        super.addView(li.inflate(layoutId, null));
     }
 
     @Override
     public void addView(View child) {
         throw new RuntimeException("adding views is not supported");
-    }
-
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        inflatedChild.layout(l, t, r, b);
     }
 }
